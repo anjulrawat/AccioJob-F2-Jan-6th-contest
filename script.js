e3eb679e1b1d23bd57436bfc49401987f2d8db95
Anjul Rawat <107921182+anjulrawat@users.noreply.github.com>
2023-01-07 23:14:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,49 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
-}
+arr.map(function(value){
+  if(value.profession === "developer")
+  {
+    console.log(value);
+  }
+})
+} 
 
 function PrintDeveloperbyForEach() {
   //Write your code here
-}
+  arr.forEach(function(value){
+    if(value.profession === "developer")
+    {
+      console.log(value);
+    }
+  })
+  } 
+  
 
 function addData() {
-  //Write your code here
+  let arr2 = [
+    {id:4,name:"susan",age:"20",profession:"intern"}
+  ];
+  let arr3 = arr.concat(arr2);
+console.log(arr3);
 }
 
 function removeAdmin() {
   //Write your code here
+  let professionToRemove = "admin" ;
+
 }
 
 function concatenateArray() {
   //Write your code here
+  let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ]; 
+  let arr2 = [
+    {id:4,name:"susan",age:"20",profession:"intern"}
+  ];
+  let result = arr.concat(arr2);
+  console.log(result);
 }
